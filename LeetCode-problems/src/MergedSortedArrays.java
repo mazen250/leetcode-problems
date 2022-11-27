aad9d07d2a077319884a80b379b98916ef59fac0
mazen250 <mazenfayez56@gmail.com>
2022-11-28 00:58:24 +0200

    RemoveDuplicates and MergedSortedArray not finished yet

---
+++ LeetCode-problems/src/MergedSortedArrays.java
@@ -0,0 +1,8 @@
+public class MergedSortedArrays {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int sum = m+n;
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+}
